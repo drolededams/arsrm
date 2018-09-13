0ed22ca5e68ad56851a34c4f883368d1282d7ee9
Damien GAMEIRO <dgameiro@e1r8p1.42.fr>
2018-09-13 14:04:14 +0200

    null fix

--- srcs/ft_memcpy.s
+++ srcs/ft_memcpy.s
@@ -5,9 +5,15 @@ section .text
 _ft_memcpy:
 	xor rax, rax
 	cmp rdi, 0
-	ret
+	je return
+	cmp rsi, 0
+	je return
 	mov rax, rdi
 	mov rcx, rdx
 	cld
 	rep movsb
+	ret
+
+return:
+	mov rax, rdi
 	ret
